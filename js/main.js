258e6bb8f18dd0ef6ec9058bf3012b891e7f528d
yatsenko0001 <80518180+yatsenko0001@users.noreply.github.com>
2021-03-23 22:19:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.logo2').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeftBig");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.text-footer').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+})
