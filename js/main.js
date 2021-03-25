82f354712b20b8a55d1d9e33f2eb1d46e513e0b5
yatsenko0001 <80518180+yatsenko0001@users.noreply.github.com>
2021-03-25 16:07:10 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
-$(function () {
-	$(window).scroll(function() {
-	    $('.logo2').each(function(){
-	        var imagePos = $(this).offset().top;
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInLeftBig");
-	        }
-	    });
-	});
-	$(window).scroll(function() {
-	    $('.text-footer').each(function(){
-	        var imagePos = $(this).offset().top;
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
+
+var btn = document.getElementById('btn');
+btn.onclick = function () { 
+ var text = document.querySelector('p.intro');
+ text.classList.add('red');
+ var img = document.querySelector('desktop');
+ img.style.display = 'none';
+
+ document.querySelector('word3').style.marginLeft ='50px'
+}
+
+
+$(function () { 
+$(window).scroll(function() {
+	$('.word-text').each(function(){
+		var imagePos = $(this).offest().top;
+
+		var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+650) {
+			$(this).addClass("bounceInLeft");
+		}
 	});
-})
+});
+
+});
+
+
+
+$(function () { 
+$(window).scroll(function() {
+	$('.word-title').each(function(){
+		var imagePos = $(this).offest().top;
+
+		var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+650) {
+			$(this).addClass("bounceInDown");
+		}
+	});
+});
+
+});
